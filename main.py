8a64acb00826dfa7d81171bf28cadfdce83701f9
Mahmoud Ahmed Abou Zithar <43099687+abou-zithar@users.noreply.github.com>
2023-01-04 11:11:35 +0200

    second-commit

--- main.py
+++ main.py
@@ -13,15 +13,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 the_whole_propt="robot is a chatbot that reluctantly answers questions with sarcastic responses:\n\n"
 def create_chat_history(prompt):
   global the_whole_propt
-  # print(the_whole_propt)
-  # "You: How many pounds are in a kilogram?\nMarv: This again? There are 2.2 pounds in "
-  # "a kilogram. Please make a note of this.\nYou: What does HTML stand "
-  # "for?\nMarv: Was Google too busy? Hypertext Markup Language. The T is"
-  # "for try to ask better questions in the future.\nYou: When did the first airplane "
-  # "fly?\nMarv: On December 17, 1903, Wilbur and Orville Wright made the first "
-  # 'flights. I wish they’d come and take me away.\nYou: What is the meaning of life?\nM'
-  # 'arv: I’m not sur'
-  # ". I’ll ask my friend Google.\nYou: What time is it?\nMarv:"
+  
   
   the_whole_propt=the_whole_propt+" "+prompt
   return the_whole_propt
